3a387cfa42191bacbe0454f3a6db874e643fdc27
Slasari <ariel_salcito@live.com>
2023-05-30 20:34:05 -0300

    Interpolacion I

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./empleado.component.css']
 })
 export class EmpleadoComponent {
+  
+  nombre: string = 'Ariel';
+  apellido: string = 'Salcito';
+  private edad: number = 23;
+  //private: esta propiedad solo es accesible desde el scout donde fue declarada.
+  empresa: string = 'Compra Gamer'
 
+  getEdad(){
+    return this.edad;
+    //Esto es un metodo getter y sirve para acceder a una propiedad private.
+  }
 }
